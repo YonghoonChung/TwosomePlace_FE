ce794294c7779f6bca85bb1b7e6da875f4c5757b
JNjeong <wnsghd0902@gmail.com>
2022-02-19 02:52:58 +0900

    진짜 미쳤다. 이걸 해냈습니다.
    슬라이드 1차 구현
    상세 디자인 추후 변경예정

--- js/appintro.js
+++
@@ -1,6 +0,0 @@
-import '../swiper/js/swiper.esm.js';
-import "../swiper/js/swiper.js"
-
-const swipercontainer = document.getElementsByClassName("swiper-container");
-
-var swiper = new Swiper(swipercontainer, {speed:500, pagination: {el: 'pagination', type:'bullets',clickable:'true',}, on: {slideChange: function(){console.log("슬라이드가 변경되었습니다.");}}});
